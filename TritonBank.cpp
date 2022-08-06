9a7a2d1cf5541a1769890cce4802c2722f451604
SirReadsALot <72465167+SirReadsALot@users.noreply.github.com>
2022-08-06 18:33:00 +0400

    Update TritonBank.cpp

--- TritonBank.cpp
+++ TritonBank.cpp
@@ -8,39 +8,20 @@
 #endif
 using namespace std;
 
+class Account {
+    public:
+        char name[10];
+        int number;
+        int deposit;
+     
+    void create_account();
+    void deposit();
+    void withdraw();
+    void show_accData();
+};
+
 int main() {
-    int passwords[5] = { 124816, 123456,  10101, 90909, 123};
-    // create account stuff
-    int createPassword[0];
-    string createUsername;
-
-    cout << "~~~~~~~~~~~~ BANK OF TRITON ~~~~~~~~~~~~ " << "\n";
-    string accName;
-    int password;
-    cout << "Enter your account information" << "\n";
-    cout << "Username: ";
-    cin >> accName;
-    cout << "Password: ";
-    cin >> password;
-    
-    while (password != 124816) {
-        cout << "Invalid Password" << "\n";
-        cout << "Password: ";
-        cin >> password;
-        if (password == 124816) {
-            cout << "You have successfully logged in!" << "\n";
-            ofstream Credentials("credentials.txt");
-            // declaring argument of time()
-            time_t my_time = time(NULL);
-  
-            // ctime() used to give the present time
-            //printf("%s", ctime(&my_time));
-            return 0;
-            Credentials << "Account Name: " + accName << "\n" << "Password: " + password;
-            Credentials.close();
-
-            Sleep(5000);
-            break;
-        } 
+    void Account::create_account() {
+        cout << "\n";
     }
 }

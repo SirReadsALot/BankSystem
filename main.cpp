cca974802f3644bebe381ae9c186ce159a34eb1e
SirReadsALot <72465167+SirReadsALot@users.noreply.github.com>
2025-02-26 14:35:04 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+
 string password = "rintoh40";
 string input;
-string var1;
-int var2;
-double var3;
+int op;
+int wA;
+int dA;
+int balance;
+int u = 0;
 
 void passwordInp() {
     cout << "Please enter the password: ";
@@ -22,17 +24,28 @@ int main() {
   while (input != password) {
     passCheck();
   }
-  if (input == password) {
-    cout << "Access granted. Please enter your variables: " << endl;
-    
-    cout << "Account: \n";
-    cin >> var1;
-    cout << "Deposit: \n";
-    cin >> var2;
-    cout << "?Security?: \n";
-    cin >> var3;
 
-    cout << "Deposit made successfully" << endl;
+    
+  if (input == password) {
+      cout << "Enter an option.\n1-Deposit\n2-Withdraw\n3-Check Balance\n4-Exit";
+      while (u == 0) {   
+          if (op == 1) {
+              cout << 'Enter amount to deposit:\n';
+              cin >> dA;
+          } else if (op == 2) {
+            cout << 'Enter amount to withdraw:\n';
+            cin >> wA;
+            if (wA <= balance) {
+                balance += wA;
+            } 
+          } else if (op == 3) {
+              cout << balance;
+          } else if (op == 4) {
+              u++;
+          } else {
+              cout << "Enter valid option.\n";
+          }
+      }
   } 
   return 0;
 }
